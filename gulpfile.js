cbf695e4ce4310eec00f89b5423ed6248abd5ab5
Oreoluwa Aboluwarin <oreoluwa.aboluwarin@andela.com>
2017-05-06 10:05:10 +0100

    add gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 const gulp = require('gulp');
+const sass = require('gulp-sass');
 const bs = require('browser-sync').create();
 
 gulp.task('browserSync', () => {
@@ -6,6 +7,19 @@ gulp.task('browserSync', () => {
     server: {
       baseDir: "./"
     },
-    proxy: "localhost:7777"
   });
 });
+
+gulp.task('build-css', () => {
+  return gulp.src('src/style/style.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('public/stylesheets'))
+    .pipe(bs.reload({ stream: true}));
+});
+
+gulp.task('watch', ['browserSync'], () => {
+  gulp.watch('src/style/style.scss', ['build-css'])
+  gulp.watch('*.html').on('change', bs.reload);
+});
+
+gulp.task('default', ['watch'])

a02dd932499faf03ebe9c62da2e22f37e6207f2b
Oreoluwa Aboluwarin <oreoluwa.aboluwarin@andela.com>
2017-05-06 10:21:58 +0100

    chore: add favicon

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const bs = require('browser-sync').create();
 
+// BrowserSync task
 gulp.task('browserSync', () => {
   bs.init({
     server: {
@@ -10,6 +11,7 @@ gulp.task('browserSync', () => {
   });
 });
 
+// Sass task
 gulp.task('build-css', () => {
   return gulp.src('src/style/style.scss')
     .pipe(sass())
@@ -17,9 +19,11 @@ gulp.task('build-css', () => {
     .pipe(bs.reload({ stream: true}));
 });
 
+// Watch task
 gulp.task('watch', ['browserSync'], () => {
   gulp.watch('src/style/style.scss', ['build-css'])
   gulp.watch('*.html').on('change', bs.reload);
 });
 
+// Default task
 gulp.task('default', ['watch'])
